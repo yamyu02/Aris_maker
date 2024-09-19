23d8cd72967b868ed66145dc745b9429a76a57e3
yamyu02 <alex.yang1255@gmail.com>
2024-09-18 22:03:32 -0400

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementx;
+
+    [SerializeField]
+    private Rigidbody2D mybody;
+    private Animator ani;
+    private SpriteRenderer sr; 
+
+    private string Walk_animation = "Walk";
+    private string Ground_Tag = "Ground"; 
+    private bool Grounded = true; 
+    private void Awake()
+    {
+        
+        mybody = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();    
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+        PlayerJump();
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+    void PlayerMoveKeyboard ()
+    {
+        movementx = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementx, 0f, 0f) * Time.deltaTime * moveForce; 
+    }
+    
+    void AnimatePlayer()
+    {
+        
+        if (movementx > 0)
+        {
+            ani.SetBool(Walk_animation, true);
+            sr.flipX = false;
+        }
+        else if (movementx < 0)
+        {
+            ani.SetBool(Walk_animation, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            ani.SetBool(Walk_animation, false);
+        }
+
+ 
+    }
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && Grounded)
+        {
+            Grounded = false;
+            mybody.AddForce(new Vector2 (0f, jumpForce), ForceMode2D.Impulse);
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(Ground_Tag))
+        {
+            Grounded = true;
+        }
+    }
+} // class
+ 
