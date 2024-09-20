75c198e57db1574a94f3d74b4f2a8f960a55c454
JasonLin5852 <160035581+JasonLin5852@users.noreply.github.com>
2024-09-20 08:54:36 -0400

    Projectiles Destroy
    Projectiles destroy themselves when attacking the player, removing 1 hp

--- Assets/Scripts/EnemyBullet.cs
+++ Assets/Scripts/EnemyBullet.cs
@@ -32,7 +32,7 @@ public class EnemyBullet : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
